68adae0682291a698a8dd23cf2634d1e000cd9a9
Dayne dos Santos Pacheco <dayne.pacheco@pucpr.edu.br>
2024-08-08 20:24:27 -0300

    criando variaveis mutaveis, constantes

--- test1/src/main.rs
+++ test1/src/main.rs
@@ -1,3 +1,10 @@
+const VALOR: u8 = 123;//como criar constantes
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5; 
+    // let é uma variavel que pode alterar o valor porém precisa do mut
+    println!("o valor de x é: {x}");
+    x = 6;
+    println!("o valor de x é: {VALOR}");
 }
